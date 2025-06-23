e297dd9e62f3c4f811b5e87602b6c7f5bf009509
huncholane <huncho@hygo.ai>
2025-06-23 14:08:07 -0700

    build main.go instead of just main

--- buildspec/build.sh
+++ buildspec/build.sh
@@ -3,19 +3,22 @@ set -e
 source ./buildspec/global.sh
 
 echo "Building api"
-cd api
-go build main
-cd ..
+(
+	cd api
+	go build main.go
+)
 echo "Successfully built api"
 
 echo "Building frontend"
-cd frontend
-npm run build
-cd ..
+(
+	cd frontend
+	npm run build
+)
 echo "Successfully built frontend"
 
 echo "Migrating database"
-cd database
-sh manage.sh migrate
-cd ..
+(
+	cd database
+	sh manage.sh migrate
+)
 echo "Successfully migrated"
