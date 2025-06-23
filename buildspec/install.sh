fb62fa67aa7fc9d0dcfd2c9b710a30915a2b9bfe
huncholane <huncho@hygo.ai>
2025-06-23 14:35:38 -0700

    fix typo in go tar

--- buildspec/install.sh
+++ buildspec/install.sh
@@ -15,7 +15,7 @@ echo "Updating go"
 (
 	cd install
 	curl -LO https://go.dev/dl/go1.24.4.darwin-amd64.tar.gz
-	rm -rf /usr/local/go && tar -c /usr/local -xzf go1.24.4.darwin-amd64.tar.gz
+	rm -rf /usr/local/go && tar -C /usr/local -xzf go1.24.4.darwin-amd64.tar.gz
 )
 go version
 
