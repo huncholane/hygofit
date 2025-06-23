61108709522b376aedffecf7b45c8a46144f2747
huncholane <huncho@hygo.ai>
2025-06-23 14:42:19 -0700

    again

--- buildspec/install.sh
+++ buildspec/install.sh
@@ -8,16 +8,15 @@ aws s3 cp s3://hygo.secrets/hygo.pem "$PEM_FILE"
 aws s3 cp s3://hygo.secrets/hygofit.env "$ENV_FILE"
 chmod 400 /tmp/hygo.pem
 
-mkdir install
-
 # Update go
 echo "Updating go"
 (
-	cd install
-	curl -LO https://go.dev/dl/go1.24.4.linux-amd64.tar.gz
-	rm -rf /usr/local/go && tar -C /usr/local -xzf go1.24.4.darwin-amd64.tar.gz
+	if [ -d install/go ]; then exit 0; fi
+	mkdir -p install/go
+	cd install/go
+	curl -LO https://go.dev/dl/go1.24.4.linux-amd64.tar.gz >/dev/null
+	rm -rf /usr/local/go && tar -C /usr/local -xzf go1.24.4.linux-amd64.tar.gz
 )
-go version
 
 # Update node
 echo "Updating node"
@@ -25,7 +24,6 @@ echo "Updating node"
 	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash >/dev/null
 	source "$HOME/.nvm/nvm.sh"
 	nvm install 22 >/dev/null
-	node -v
 )
 
 source buildspec/global.sh
