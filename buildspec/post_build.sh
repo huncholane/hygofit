47500e47d78ae83561785fe2b8477d093f8f63c9
huncholane <huncho@hygo.ai>
2025-06-24 05:22:06 -0700

    using systemd on server

--- buildspec/post_build.sh
+++ buildspec/post_build.sh
@@ -7,13 +7,10 @@ aws s3 sync frontend/out s3://hygofit.frontend --delete >/dev/null
 
 ls "$PEM_FILE"
 
-echo "Making hygofit directory"
-dossh "mkdir -p ~/hygofit"
+echo "Copying the service file"
+scpto api/hygofit-api.service /etc/systemd/system/hygofit-api.service
 echo "Copying main to server"
-scpto api/main hygofit/main
+scpto api/main /usr/local/bin/hygofit
 echo "Copying env to server"
-scpto $ENV_FILE hygofit/.env
-echo "copying start.sh to server"
-scpto api/start.sh hygofit/start.sh
-echo "starting the server"
-dossh 'cd hygofit && sh start.sh'
+scpto $ENV_FILE /etc/hygofit.env
+dossh 'sudo systemctl enable hygofit-api --now && sudo systemctl restart hygofit-api'
